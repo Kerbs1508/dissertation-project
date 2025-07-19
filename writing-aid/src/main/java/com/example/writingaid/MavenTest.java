9289d143f098471c71ffcaa6650fd1935c432bd4
Ayesha Kerbelker <ayeshak@Ayeshas-MacBook-Pro.local>
2025-07-19 22:22:57 +0100

    Initial commit of my dissertation code

---
+++ writing-aid/src/main/java/com/example/writingaid/MavenTest.java
@@ -0,0 +1,8 @@
+package com.example.writingaid;
+
+import org.languagetool.JLanguageTool;
+
+public class MavenTest {
+    private JLanguageTool lt;
+}
+// imports and declares language tool 
---
+++ writing-aid/src/main/java/com/example/writingaid/WritingAidApplication.java
@@ -0,0 +1,13 @@
+package com.example.writingaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritingAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritingAidApplication.class, args);
+	} //Spring Boot build program
+
+}
